a1dda71e31fb9f836ccb86a45650b135ceb1cf89
James Rees <James.rees@outlook.com>
2021-04-19 22:05:35 +0100

    Converted knight class

--- src/knight.cpp
+++ src/knight.cpp
@@ -6,12 +6,11 @@ Knight::Knight(SDL_Texture *_texture, SDL_Texture *_idleTexture)
 {
     this->texture = _texture;
     this->idleTexture = _idleTexture;
-    this->sprite.setTexture(this->idleTexture);
-    this->sprite.setTextureRect( IntRect(0, 0, 200, 200));
-    this->x = 0;
-    this->y = 0;
-    this->isIdle = true;
-    this->sprite.setScale(10, 10);
+    this->textureRect = {0, 0, 200, 200};
+    this->positionRect = {0, 0, 10, 10};
+ //   this->sprite.setTexture(this->idleTexture);
+ //   this->sprite.setTextureRect( IntRect(0, 0, 200, 200));
+  //  this->sprite.setScale(10, 10);
     this->attackAnimation = new Animator(26, 14, 120, 60);
     this->idleAnimation = new Animator(5, 7, 120, 60);
 
@@ -19,29 +18,28 @@ Knight::Knight(SDL_Texture *_texture, SDL_Texture *_idleTexture)
 
 void Knight::move(int _x, int _y)
 {
-    x += _x;
-    y += _y;
-    this->sprite.move(_x, _y);
+    this->positionRect.x += _x;
+    this->positionRect.y += _y;
 };
 
 void Knight::animate(double elapsedTime)
 {
     if ( this->isIdle ) 
     {
-        this->sprite.setTexture(this->idleTexture);
-        this->sprite = this->idleAnimation->Animate(elapsedTime, this->sprite);
+        this->texture = this->idleTexture;
+        this->texture = this->idleAnimation->Animate(elapsedTime, this->texture);
     }
     else 
     {   
-        this->sprite.setTexture(this->texture);
-        this->sprite = this->attackAnimation->Animate(elapsedTime, this->sprite);
+        this->texture = this->texture;
+        this->texture = this->attackAnimation->Animate(elapsedTime, this->texture);
     
     }
 };
 
 void Knight::render(SDL_Renderer *_renderer)
 {   
-    window.draw(this->sprite);
+    SDL_RenderCopy(_renderer, this->texture, &this->textureRect, &this->positionRect);
 };
 
 void Knight::setIdle(bool _idle)
